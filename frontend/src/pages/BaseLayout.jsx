03ab7b0d1dc1b1cba48c2b0622e177b401b77fe8
Sebastian Vega <sebasvega4604@gmail.com>
2026-03-22 23:55:19 -0400

    feat: added all pages and basic components

---
+++ frontend/src/pages/BaseLayout.jsx
@@ -0,0 +1,15 @@
+import Header from "@components/header"
+import Footer from "@components/footer"
+import { Outlet } from "react-router-dom"
+
+function BaseLayout() {
+    return (
+        <>
+            <Header />
+            <Outlet />
+            <Footer />
+        </>
+    );
+}
+
+export default BaseLayout;
