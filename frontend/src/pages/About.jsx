286178a4acd5c66028e0df101f90d2f1552e594f
Sebastian Vega <sebasvega4604@gmail.com>
2026-03-24 00:25:46 -0400

    feat: began skills styles

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,3 +1,4 @@
+import styles from "@styles/about.module.css";
 import experience from "@data/experience.json";
 import skills from "@data/skills.json";
 
@@ -7,28 +8,28 @@ function About() {
             <section>
                 <div></div>
             </section>
-            <section>
-                <div>
+            <section id={styles.skills_section}>
+                <div id={styles.skills_container}>
                     <h2>My Skills</h2>
-                    <ul>
+                    <ul id={styles.skills_list}>
                         {skills.map((skill) => (
-                            <li key={skill.name}>
+                            <li key={skill.name} className={styles.skill_item}>
                                 <img src={skill.img} alt={skill.name} />
-                                <span>{skill.name}</span>
+                                <p>{skill.name}</p>
                             </li>
                         ))}
                     </ul>
                 </div>
             </section>
-            <section>
-                <div>
+            <section id={styles.experience_section}>
+                <div id={styles.experience_container}>
                     <h2>My Experience</h2>
-                    <ul>
+                    <ul id={styles.experience_list}>
                         {experience.map((exp) => (
-                            <li key={exp.company}>
-                                <h3>{exp.company}</h3>
-                                <p>{exp.title}</p>
-                                <p>{exp.period}</p>
+                            <li key={exp.company} className={styles.experience_item}>
+                                <h3 className={styles.experience_company}>{exp.company}</h3>
+                                <p className={styles.experience_title}>{exp.title}</p>
+                                <p className={styles.experience_period}>{exp.period}</p>
                                 <img src={exp.img} alt={exp.company} />
                             </li>
                         ))}
